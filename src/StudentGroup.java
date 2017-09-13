73c71c4be1dea714f5c023c5d749c6e121f637db
kirankumarch0510 <31915772+kirankumarch0510@users.noreply.github.com>
2017-09-13 15:00:51 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -33,59 +33,121 @@ public class StudentGroup implements StudentArrayOperation {
 	public void setStudents(Student[] students) {
 		// Add your implementation here
 		this.students=students;
-		}
+	}
 
 	@Override
 	public Student getStudent(int index) {
 		// Add your implementation here
-		
-	if(index<0){
-	throw new IllegalArgumentException();}
 		return null;
-
 	}
 
 	@Override
 	public void setStudent(Student student, int index) {
-		// Add your implementation hereif(index<0){
-			if(index<0){
-	throw new IllegalArgumentException();}
-	
+		// Add your implementation here
 	}
 
 	@Override
 	public void addFirst(Student student) {
 		// Add your implementation here
+		Student[] temp=new Student[this.students.length+1];
+		temp[0]=student;
+		for(int i=1;i<temp.length;i++)
+		{
+			temp[i]=this.students[i-1];
+		}
+		this.students=temp;
 	}
 
 	@Override
 	public void addLast(Student student) {
 		// Add your implementation here
+		Student[] temp=new Student[this.students.length+1];
+		int i;
+		for( i=0;i<temp.length-1;i++)
+		{
+			temp[i]=this.students[i];
+		}
+		temp[i]=student;
+		this.students=temp;
 	}
 
 	@Override
 	public void add(Student student, int index) {
 		// Add your implementation here
+		Student[] temp=new Student[this.students.length+1];
+		for(int i=0; i<index-1; i++)
+		{
+			temp[i]=this.students[i];
+		}		
+		temp[index-1]=student;
+		for(int i=index; i<this.students.length; i++)
+		{
+			temp[i+1]=this.students[i];
+		}
+		this.students=temp;
 	}
 
 	@Override
 	public void remove(int index) {
 		// Add your implementation here
+		Student[] temp=new Student[this.students.length-1];
+		int c=0;
+		for(int i=0; i<this.students.length; i++)
+		{
+			if(i!=index)
+			{
+				temp[c++]=this.students[i];
+			}
+		}
+		this.students=temp;
+		 
 	}
 
 	@Override
 	public void remove(Student student) {
 		// Add your implementation here
+		Student[] temp=new Student[this.students.length-1];
+		int c=0;
+		for(int i=0; i<this.students.length; i++)
+		{
+			if(this.students[i]!=student)
+			{
+				temp[c++]=this.students[i];
+			}
+		}
+		this.students=temp;
 	}
 
 	@Override
 	public void removeFromIndex(int index) {
 		// Add your implementation here
+		Student[] temp=new Student[this.students.length-1];
+		int c=0;
+		for(int i=0; i<this.students.length; i++)
+		{
+			temp[c++]=this.students[i];
+			if(i==index-1)
+			{
+				break;
+			}
+		}
+		this.students=temp;
 	}
 
 	@Override
 	public void removeFromElement(Student student) {
 		// Add your implementation here
+		Student[] temp=new Student[this.students.length-1];
+		int c=0;
+		for(int i=0; i<this.students.length; i++)
+		{
+			temp[c++]=this.students[i];
+			if(this.students[i]==student)
+			{
+				break;
+			}
+		}
+		this.students=temp;
 	}
 
 	@Override
@@ -101,6 +163,18 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void bubbleSort() {
 		// Add your implementation here
+		for(int i = 0; i < this.students.length; i++)
+		  {
+			  for(int j = 0; j < this.students.length-i-1; j++)
+			  {
+                   if(this.students[j].getId() > this.students[j+1].getId())
+				   {
+					   Student temp = this.students[j];
+					   this.students[j] = this.students[j+1];
+					   this.students[j+1] = temp;
+				   }
+			  }	   
+		  }
 	}
 
 	@Override
